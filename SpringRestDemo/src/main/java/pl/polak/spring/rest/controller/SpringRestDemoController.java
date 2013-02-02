beda3670c70fb034d4d3f3fdb852587a87253d92
polok <polok>
2013-02-03 00:02:56 +0100

    *load data from database

--- SpringRestDemo/src/main/java/pl/polak/spring/rest/controller/SpringRestDemoController.java
+++ SpringRestDemo/src/main/java/pl/polak/spring/rest/controller/SpringRestDemoController.java
@@ -3,6 +3,7 @@ package pl.polak.spring.rest.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,16 +16,13 @@ import pl.polak.spring.rest.service.SpringRestDemoService;
 @RequestMapping("/patient")
 public class SpringRestDemoController {
 
+	@Autowired
 	private SpringRestDemoService service;  
 	
 	@ResponseBody
 	@RequestMapping(value = "/", method = RequestMethod.GET, produces = "application/json")
 	public List<Patient> getAllPatientsAsJson() {
-		List<Patient> patients = new ArrayList<Patient>();
-		patients.add(new Patient("Jan", "Kowalski", 30));
-		patients.add(new Patient("Piotr", "Kowalczyk", 25));
-		patients.add(new Patient("Jacek", "Mazur", 22));
-		return patients;
+		return service.getAllPatients();
 	}
 	
 }
--- SpringRestDemo/src/main/java/pl/polak/spring/rest/service/SpringRestDemoService.java
+++ SpringRestDemo/src/main/java/pl/polak/spring/rest/service/SpringRestDemoService.java
@@ -1,8 +1,11 @@
 package pl.polak.spring.rest.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import pl.polak.spring.rest.domain.Patient;
 import pl.polak.spring.rest.repository.SpringRestDemoRepository;
 
 @Service
@@ -10,4 +13,8 @@ public class SpringRestDemoService {
 
 	@Autowired
 	private SpringRestDemoRepository repository;
+	
+	public List<Patient> getAllPatients() {
+		return (List<Patient>) repository.findAll();
+	}
 }
